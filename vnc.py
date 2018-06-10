481a58ad96af3db10b58471d5acada38c5ebab7e
prazd <alexokda@list.ru>
2018-06-10 20:50:27 +0300

    vnc

--- vnc.py
+++ vnc.py
@@ -4,28 +4,28 @@ from threading import Thread
 from tkinter import messagebox
 import re
 
-
 root = Tk()
 q = Frame()
 
-root.title('SSH')
-root.geometry('500x400')
-Label(root, text = 'VAR1').pack()
+root.title('VNC')
+root.geometry('300x200')
+#Label(root, text = 'VAR1').pack()
 inp = Entry(width=15)   # Ввод для Var2
 alt = [1]
+inpv = Entry(width=3)   # Ввод для Var1
+inpv.place(x = 140, y = 20)
 
-sv = StringVar(root)
-Label(root, text = 'ALT').place(x = 100, y=25)
-opt = OptionMenu(root,sv,*alt)
-opt.pack()
+Label(root, text = 'ws-alt').place(x = 100, y=20)
+#opt = OptionMenu(root,sv,*alt)
+#opt.place(x = 50, y = 10)
+#sv = StringVar(root)
 
 def var1():
     def c():
        try:
-            q = sv.get()
-            subprocess.call('ssh -fN -L 5900:localhost:5900 alex@' + q,shell=True)
+            q = inpv.get()
+            subprocess.call('ssh -fN -L 5900:localhost:5900 ws-alt-' + q,shell=True)
             subprocess.call('vncviewer localhost',shell=True)
-       #messagebox.showinfo("Information","Informative message")
        finally:
              try:
                   q = subprocess.check_output('ps ax | grep 5900 |grep -v grep|awk \'{print $1}\'',shell=True)
@@ -39,7 +39,7 @@ def var1():
 
 
 def ping():
-     q = sv.get()
+     q = 'name_of_host'+inpv.get()#number
      try:
          a = subprocess.check_output('ping -c 1 ' + q, shell=True)
          print('OK')
@@ -50,21 +50,18 @@ def ping():
          l['text'] = 'BAD'
          l['bg'] = 'red'
 
-
-
-
 buttonping = Button(root, text='ping', command=ping)
-buttonping.place(x = 450, y = 25)
+buttonping.place(x = 250, y = 25)
 
 
 l = Label(width=3)
-l.place(x=470, y=5)
+l.place(x=270, y=5)
 
-Label(root, text = '').pack()
-Label(root, text = 'VAR2').pack()
+#Label(root, text = '').pack()
+#Label(root, text = 'VAR2').pack()
 
-Label(root, text='vncviewer ').place(x=110,y=96)
-inp.pack()
+#Label(root, text='ws-alt').place(x=10,y=90)
+inp.place(x = 50, y = 90)
 
 def var2():
    def c():
@@ -98,19 +95,16 @@ def ping2():
          l2['bg'] = 'red'
 
 
-
 button = Button(root, text='Connect',command=var1)
-button.place(x = 380, y = 20, height=35, width=59)
-
+button.place(x = 180, y = 20, height=35, width=59)
 
 q.pack()
 l2 = Label(width=3)
-l2.place(x=400, y=70)
+l2.place(x=270, y=65)
 
 button2 = Button(root, text='Connect',command=var2)
-button2.place(x = 350, y = 85, height=35, width=59)
+button2.place(x = 180, y = 80, height=35, width=59)
 
 buttonping2 = Button(root, text='ping', command=ping2)
-buttonping2.place(x = 430, y = 85)
-
+buttonping2.place(x = 250, y = 85)
 root.mainloop()
